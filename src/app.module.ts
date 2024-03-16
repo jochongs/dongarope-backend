77a47105ec174e16e11a682e8010801e89564a4c
jochongs <joch2712@naver.com>
2024-03-16 14:15:40 +0900

    chore: create basic module structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './api/auth/auth.module';
+import { GenerationModule } from './api/generation/generation.module';
+import { GroupModule } from './api/group/group.module';
+import { QuestionModule } from './api/question/question.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, GenerationModule, GroupModule, QuestionModule],
 })
 export class AppModule {}
